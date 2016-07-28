cadb58e35682482e01f063b2d7f4760f84318282
Eric Casteleijn <thisfred@gmail.com>
2016-07-28 10:24:49 -0700

    add context menu and play/pause keys to main layout

--- keyboards/preonic/keymaps/thisfred/keymap.c
+++ keyboards/preonic/keymaps/thisfred/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Play | Ctrl | GUI  | Alt  |Lower |    Space    |Raise | Ctrl | Alt  |      |      |
+ * | Menu | Ctrl | GUI  | Alt  |Lower |    Space    |Raise | Ctrl | Alt  |      | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ESC},
   {KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
-  {KC_MPLY, KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RCTL, KC_RALT, _______, _______}
+  {KC_APPLICATION, KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RCTL, KC_RALT, KC_EXECUTE, KC_MPLY}
 },
 
 /* Colemak
